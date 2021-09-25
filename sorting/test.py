2fe3c1c0e1814b25edacc231b146ce31ae950859
Thomas Lee <thomaslee9474@gmail.com>
2021-09-24 18:29:17 -0700

    [fix] test

--- sorting/test.py
+++ sorting/test.py
@@ -1,6 +1,7 @@
 from merge_sort import merge_sort
 from quick_sort import quick_sort, random_partition, \
-    median_partition, bfort_partition
+    median_partition, bfort_partition, first_partition, \
+    median_random_partition
 
 
 def test_merge_sort(nums, sorted_nums):
@@ -8,10 +9,34 @@ def test_merge_sort(nums, sorted_nums):
     assert merge_sort(nums) == sorted_nums
 
 
+# =========================================
 def test_quick_sort(nums, sorted_nums):
+    test_first_parition(nums, sorted_nums)
+    test_random_partition(nums, sorted_nums)
+    test_median_partition(nums, sorted_nums)
+    test_median_random_partition(nums, sorted_nums)
+    test_bfort_partition(nums, sorted_nums)
+
+
+def test_first_parition(nums, sorted_nums):
+    assert quick_sort(nums, first_partition) == sorted_nums
+
+
+def test_random_partition(nums, sorted_nums):
     assert quick_sort(nums, random_partition) == sorted_nums
+
+
+def test_median_partition(nums, sorted_nums):
     assert quick_sort(nums, median_partition) == sorted_nums
+
+
+def test_median_random_partition(nums, sorted_nums):
+    assert quick_sort(nums, median_random_partition) == sorted_nums
+
+
+def test_bfort_partition(nums, sorted_nums):
     assert quick_sort(nums, bfort_partition) == sorted_nums
+# =========================================
 
 
 if __name__ == "__main__":
