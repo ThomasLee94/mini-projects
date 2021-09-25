e978b595b51ef92939f7a6e532dc1f480672c39a
Thomas Lee <thomaslee9474@gmail.com>
2021-09-24 18:17:35 -0700

    [add] tests

---
+++ sorting/test.py
@@ -0,0 +1,22 @@
+from merge_sort import merge_sort
+from quick_sort import quick_sort, random_partition, \
+    median_partition, bfort_partition
+
+
+def test_merge_sort(nums, sorted_nums):
+    assert merge_sort(nums) == sorted_nums
+    assert merge_sort(nums) == sorted_nums
+
+
+def test_quick_sort(nums, sorted_nums):
+    assert quick_sort(nums, random_partition) == sorted_nums
+    assert quick_sort(nums, median_partition) == sorted_nums
+    assert quick_sort(nums, bfort_partition) == sorted_nums
+
+
+if __name__ == "__main__":
+    nums = [9, 4, 3, 5, 1, 2, 8, 6]
+    sorted_nums = [1, 2, 3, 4, 5, 6, 8, 9]
+
+    test_merge_sort(nums, sorted_nums)
+    test_quick_sort(nums, sorted_nums)
